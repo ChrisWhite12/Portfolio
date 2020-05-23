0dbf8b368f99fe56e31446aef81063ded2ae152f
ChrisWhite12 <chris_white_12@hotmail.com>
2020-05-23 17:20:58 +1000

    added animations to skills icons, links to social icons

--- projects.js
+++ projects.js
@@ -1,43 +1,37 @@
+ruby_i = document.querySelector(".ruby_icon");
+html_i = document.querySelector(".html_icon");
+css_i = document.querySelector(".css_icon");
+js_i = document.querySelector(".js_icon");
+
 document.querySelector(".project1").addEventListener('mouseover',el => {
-    ruby = document.querySelector(".ruby_icon");
-    ruby.style = 'filter: grayscale(0%);';
+    ruby_i.style = 'animation: icon_hover_in 1s linear 0s 1 normal both;';
 })
 
 document.querySelector(".project1").addEventListener('mouseleave',el => {
-    ruby = document.querySelector(".ruby_icon");
-    ruby.style = 'filter: grayscale(100%);';
+    ruby_i.style = 'animation: icon_hover_out 1s linear 0s 1 normal both;';
 })
 //----------------------------------------------------------------------------
 document.querySelector(".project3").addEventListener('mouseover',el => {
-    html_i = document.querySelector(".html_icon");
-    html_i.style = 'filter: grayscale(0%);'; 
-    css_i = document.querySelector(".css_icon");
-    css_i.style = 'filter: grayscale(0%);';
-    js_i = document.querySelector(".js_icon");
-    js_i.style = 'filter: grayscale(0%);';
+    
+    html_i.style = 'animation: icon_hover_in 1s linear 0s 1 normal both;'; 
+    css_i.style = 'animation: icon_hover_in 1s linear 0s 1 normal both;';
+    js_i.style = 'animation: icon_hover_in 1s linear 0s 1 normal both;';
 })
 
 document.querySelector(".project3").addEventListener('mouseleave',el => {
-    html_i = document.querySelector(".html_icon");
-    html_i.style = 'filter: grayscale(100%);';
-    css_i = document.querySelector(".css_icon");
-    css_i.style = 'filter: grayscale(100%);';
-    js_i = document.querySelector(".js_icon");
-    js_i.style = 'filter: grayscale(100%);';
+    html_i.style = 'animation: icon_hover_out 1s linear 0s 1 normal both;';
+    css_i.style = 'animation: icon_hover_out 1s linear 0s 1 normal both;';
+    js_i.style = 'animation: icon_hover_out 1s linear 0s 1 normal both;';
 })
 //-----------------------------------------------------------------------------
 document.querySelector(".project2").addEventListener('mouseover',el => {
-    html_i = document.querySelector(".html_icon");
-    html_i.style = 'filter: grayscale(0%);'; 
-    css_i = document.querySelector(".css_icon");
-    css_i.style = 'filter: grayscale(0%);';
+    html_i.style = 'animation: icon_hover_in 1s linear 0s 1 normal both;'; 
+    css_i.style = 'animation: icon_hover_in 1s linear 0s 1 normal both;';
 })
 
 document.querySelector(".project2").addEventListener('mouseleave',el => {
-    html_i = document.querySelector(".html_icon");
-    html_i.style = 'filter: grayscale(100%);';
-    css_i = document.querySelector(".css_icon");
-    css_i.style = 'filter: grayscale(100%);';
+    html_i.style = 'animation: icon_hover_out 1s linear 0s 1 normal both;';
+    css_i.style = 'animation: icon_hover_out 1s linear 0s 1 normal both;';
 })
 
     
