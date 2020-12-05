ca9d050da017d1ed8392f0e31c754ac435f21a93
ChrisWhite12 <chris_white_12@hotmail.com>
2020-12-05 10:03:15 +1000

    removing blog and contact page

--- src/main.js
+++ src/main.js
@@ -5,9 +5,7 @@ if (window.matchMedia("(max-width: 800px)").matches)        //small screen
                         <ul class="drop_menu_list">
                             <li><a href="./home.html" class="home_nav">Home</a></li>
                             <li><a href="./projects.html" class="projects_nav">Projects</a></li>
-                            <li><a href="./blog.html" class="blog_nav">Blog</a></li>
                             <li><a href="./about.html" class="about_nav">About</a></li>
-                            <li><a href="./contact.html" class="contact_nav">Contact</a></li>
                         </ul>
                     </div>`
     
@@ -19,9 +17,7 @@ else if(window.matchMedia("(min-width: 800px)").matches)    //large screen
 {
     var markup = `<li><a href="./home.html" class="home_nav">Home</a></li>
     <li><a href="./projects.html" class="projects_nav">Projects</a></li>
-    <li><a href="./blog.html" class="blog_nav">Blog</a></li>
-    <li><a href="./about.html" class="about_nav">About</a></li>
-    <li><a href="./contact.html" class="contact_nav">Contact</a></li>`
+    <li><a href="./about.html" class="about_nav">About</a></li>`
     document.querySelector(".navbar").insertAdjacentHTML('beforeend',markup)
     document.querySelector(".navbar").style = 'justify-content: space-evenly';
 }
